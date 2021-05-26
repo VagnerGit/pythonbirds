d69091286b5c4e8f5f3599b0a349fa0a5ee136f4
VagnerGit <vagnergit@gmail.com>
2021-05-26 10:59:29 -0300

    pythonBirds finalizado

---
+++ oo/pessoa.py
@@ -0,0 +1,58 @@
+class Pessoa:
+    olhos = 2 #atributo padrão
+
+    def __init__(self, *filhos, nome=None, idade=35):
+        self.idade = idade
+        self.nome = nome
+        self.filhos = list(filhos)
+
+    def cumprimentar(self):
+        return f'Olá {id(self)}'
+
+    @staticmethod
+    def metodo_estatico():
+        return 42
+
+    @classmethod
+    def nome_e_atributos_de_classe(cls):
+        return f'{cls} - olhos {cls.olhos}'
+
+
+class Homem(Pessoa):
+    def cumprimentar(self):
+        cumprimentar_da_classe = super().cumprimentar()
+        return f'{cumprimentar_da_classe}. Aperto de mão'
+
+
+class Mutante(Pessoa):
+    olhos = 3
+
+
+if __name__ == '__main__':
+    vagner = Mutante(nome='Vagner')
+    luciano = Homem(vagner, nome='Luciano')
+    print(Pessoa.cumprimentar(luciano))
+    print(id(luciano))
+    print(luciano.cumprimentar())
+    print(luciano.nome)
+    print(luciano.idade)
+    for filho in luciano.filhos:
+        print(filho.nome)
+    luciano.sobrenome = 'Ramalho'
+    del luciano.filhos
+    luciano.olhos = 1
+    del luciano.olhos
+    print(luciano.__dict__)
+    print(vagner.__dict__)
+    print(Pessoa.olhos)
+    print(luciano.olhos)
+    print(vagner.olhos)
+    print(id(Pessoa.olhos), id(luciano.olhos), id(vagner.olhos))
+    print(Pessoa.metodo_estatico(), luciano.metodo_estatico())
+    print(Pessoa.nome_e_atributos_de_classe(), luciano.nome_e_atributos_de_classe())
+    pessoa = Pessoa('Anonimo')
+    print(isinstance(pessoa, Pessoa))
+    print(isinstance(pessoa, Homem))
+    print(isinstance(vagner, Pessoa))
+    print(isinstance(vagner, Homem))
+    print(vagner.olhos)
